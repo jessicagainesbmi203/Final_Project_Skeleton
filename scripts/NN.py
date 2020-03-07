ea8d07f052f7ad063f5c06bf6bbc33e9ae05dbe6
Jessica Gaines <jessica.gaines@ucsf.edu>
2020-03-06 16:36:26 -0800

    Travis

--- scripts/NN.py
+++ scripts/NN.py
@@ -1,17 +1,19 @@
 import numpy as np
+
 class NeuralNetwork:
     def __init__(self, setup=[[68,25,"sigmoid",0],[25,1,"sigmoid",0]],lr=.05,seed=1,error_rate=0,bias=1,iter=500,lamba=.00001,simple=0):
-
+        i = 1
     def make_weights(self):
-
+        i = 1
     def feedforward(self):
-
+        i = 1
     def backprop(self):
-
-    def fit(self:
-
+        i = 1
+    def fit(self):
+        i = 1
     def predict(self):
+        i = 1
 
 
 def activation(x):
-    
+    i = 1
--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
 from scripts import NN
 
-
 def test_encoder():
+    assert 1 == 1
 
 def test_encoder_relu():
-
+    assert 1 == 1
 
 def test_one_d_ouput():
+    assert 1 == 1
