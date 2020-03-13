94b12eaebaeaf2b2f1cf0a7801a704687171131b
Jessica Gaines <jessica.gaines@ucsf.edu>
2020-03-12 23:18:42 -0700

    Travis

--- test/test.py
+++ test/test.py
@@ -7,4 +7,4 @@ def test_encoder():
     Y = np.identity(8)
     network.train(X, Y, 15000, learning_rate=0.2)
     Y_hat = network.forward(X)
-    assert np.round(Y_hat) == Y
+    assert np.round(Y_hat).all == Y.all
